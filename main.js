4acbe22b98cb2e5db63333d4220a108db996ee8e
Jarrett <jarrettfoy@gmail.com>
2022-09-16 14:34:21 -0600

    whatever

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
+let clickCount = 0
+let height = 120
+let width = 100
+let inflationRate = 20
+let maxsize = 300
+let popCount = 0
+
 function inflate() {
-  var balloonElement = document.getElementById("balloon")
-  balloonElement.style.height = "400px"
-  balloonElement.style.width = "300px"
+    clickCount++
+    let balloonElement = document.getElementById("balloon")
+    height += inflationRate
+    width += inflationRate
+    if (height >= maxsize) {
+        console.log("pop the balloon")
+        popCount++
+        height = 0
+        width = 0
+        document.getElementById("pop-count").innerText = popCount.toString()
+    }
+    
+    balloonElement.style.height = height = "px"
+    balloonElement.style.width = width = "px"
+
+
+    let clickCountElem = document.getElementById("click-count")
+    clickCountElem.innerText = clickCount.toString()
+
 }
